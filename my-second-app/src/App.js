ce663b78b742d5abd4a3aabc6bff475d47a083fb
Puneet Vashisht <puneetvashisht@outlook.com>
2021-11-09 13:03:33 +0530

    quiz app

--- my-second-app/src/App.js
+++ my-second-app/src/App.js
@@ -1,17 +1,24 @@
 // import logo from './logo.svg';
 import './App.css';
+// import AddCourse from './components/AddCourse';
 import Badge from './components/Badge';
 import BadgeWithClass from './components/BadgeWithClass';
 import Dropdown from './components/Dropdown';
+import ViewCourses from './components/ViewCourses';
+// import Quiz from './components/Quiz';
 
 function App() {
   return (
     <div>
-      <Badge caption="Inbox"></Badge>
+      {/* <Badge caption="Inbox"></Badge>
       <hr/>
       <BadgeWithClass caption="Sent"></BadgeWithClass>
       <hr/>
       <Dropdown></Dropdown>
+      <hr/> */}
+    {/* <AddCourse></AddCourse> */}
+    <ViewCourses></ViewCourses>
+     
     </div>
   );
 }
