344bff3e262b7d245b8db531023d5e0c664760f8
Puneet Vashisht <puneetvashisht@outlook.com>
2021-11-12 12:56:37 +0530

    uncontrolled forms

--- my-redux-app/src/App.js
+++ my-redux-app/src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import ViewCourses from './components/ViewCourses';
 import DisplayCourses from './components/DisplayCourses';
 import AddCourse from './components/AddCourse';
-
+import NameForm from './components/NameForm';
 
 function App() {
   return (
     <>
-
+      <NameForm></NameForm>
       <AddCourse></AddCourse>
       <hr/>
       <ViewCourses></ViewCourses>
